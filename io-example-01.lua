6918127d044c52ee9022d4145028602568349afb
Shane Yost <scy@shaneyost.io>
2025-04-04 21:42:36 -0600

    updated code examples and docs

--- io-example-01.lua
+++ io-example-01.lua
@@ -1,67 +1,80 @@
 #!/usr/bin/env luajit
---- This is considered a typical idiom to check for errors. How can we get away
---- without explicitly passing two arguments to assert?
---- 
---- First, lets understand that `io.open` may return one or two values. On 
---- failure, it would return nil and the error message. 
+
+--- A typical convention when opening a file is to use assert according to
+--- sources. On failure, nil and the error message is supplied to `assert`. On
+--- success `io.open` returns a file handle (stream) and assert returns it. I
+--- haven't read enough to know if this is a good convention outside of what
+--- the book is telling me as of now.
+local file_handle = assert(io.open("foo.txt", "r"))
+
+--- After we receive a handle we can read from or write to the stream through
+--- using methods like `read` and `write`. We call them as methods using the
+--- colon notation. Note in colon notation self is automatically passed.
+local file_contents = file_handle:read("a")
+print(file_contents)
+
+--- First question, why does type(file) return `userdata`? That doesn't mean
+--- much to me nor allows much usage. Is there a way to gather more
+--- information on the type? Am I doing something wrong?
 ---
---- Second, we need to know what assert takes. It requires one argument with an
---- optional second argument being a message.
+---@usage >lua
+---     print("type(file_handle): ", type(file_handle))
+---     file_handle:close()
+--- <
 ---
---- On failure, you're providing nil and the error message. On failure, assert
---- sees `nil` and the error message return by `io.open` will be raised.
+--- Lua provides functions designed specifically for file objects. The most
+--- common is `io.type()`. Another question now is what happens when I call
+--- `io.type()` after it's been closed? What will it print?
 ---
---- On success, `io.open` succeeds where `assert` sees a valid file handle (
---- i.e stream) and returns it.
-local f = assert(io.open("foo.txt", "r"))
-local t = f:read("a")
---- Why does type(f) return userdata? I guess I'm expecting something more
---- reflective of file stream or file handle. 
----
---- In Lua, userdata is a special data type used to represent C data from 
---- outside Lua itself. Lua's built-in file handles (`io.open`) are actually
---- implemented in C under the hood, making them appear as userdata. Userdata
---- itself doesn't tell me much about what's inside. If I really wanted to get 
---- more information what could I do?
----
---- Lua provides functions designed specifically for file objects. The most 
---- common is `io.type().
-print("type(f): ", type(f))
-print("io.type(f): ", io.type(f))
-print("type(t): ", type(t))
---- If we call `f:close` and then call `io.type(f)` it will say something like
---- "closed file".
----
---- So when would I want to use io.type(f)? Is there a real world situation 
+---@usage >lua
+---     print("io.type(file_handle): ", io.type(file_handle))
+---     file_handle:close()
+---     print("io.type(file_handle): ", io.type(file_handle))
+--- <
+--- io.type(file_handle): 	file
+--- io.type(file_handle): 	closed file
+print("io.type(file_handle): ", io.type(file_handle))
+
+--- So when would I want to use io.type(f)? Is there a real world situation
 --- where it might be useful?
 ---
---- 1. Ensuring a file is still open
---- When passing file handles around, it might be unclear if they've already
---- been closed elsewhere. So this might warrant something like the following.
-if io.type(f) == "file" then
-    -- do something
-else
-    error("File is already closed.")
-end
-f:close()
-print(t)
---- If i'm uncertain of a file's state (open or closed), `io.type` makes it 
---- safe to close files multiple times without causing errors.
---- local function safe_close(f)
----     if io.type(f) == "file" then
----         f:close()
+--- - Ensuring a file handle is still opened prior to doing something
+---
+---@usage >lua
+---     if io.type(file_handle) == "file" then
+---         -- do something
+---     else
+---         error("File is already closed.")
 ---     end
---- end
+--- <
 ---
---- I might also want to use it if i'm writing reusable Lua library that
---- expects a file handle. 
---- function process_file(f)
----     assert(io.type(f) == "file", "Expect a file handle")
---- end
+--- - Ensuring closed is not called on a already closed file handle
 ---
---- I could also use it for reopening a file if it has closed. Useful in longer
---- running programs or services that reuse file handles over extended periods.
+---@usage >lua
+---     local function safe_close(file_handle)
+---         if io.type(file_handle) == "file" then
+---             file_handle:close
+---         end
+---     end
+--- <
 ---
---- `io.type(f) is practical whenever you need robustness, clarity, or safety
---- in handling lua file handles - especially as your applications grow in 
---- complexity. 
+--- - Ensuring a file handle argument to function is a file handle
+---
+---@usage >lua
+---     local function foo(file_handle)
+---         assert(io.type(file_handle) == "file", "Expected a file handle")
+---         -- do something
+---     end
+--- <
+---
+--- - Ensuring that I can reopen a file safely unsure if it was closed
+---
+---@usage >lua
+---     local function reopen(file_handle, filename)
+---         if io.type(file_handle) ~= "file" then
+---             return io.open(filename, "r")
+---         else
+---             return file_handle
+---         end
+---     end
+--- <
